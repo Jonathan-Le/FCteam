e5f724ccd22abd5d41f57796963bd6122e6021a8
Jonathan Lee <li.junya@husky.neu.edu>
2020-04-22 12:09:15 -0400

    4.21

---
+++ SaveAnimal_Final4.21/src/Business/Role/HospitalAdminRole.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.Hospital.HospitalAdminJPanel;
+import userinterface.Hospital.HospitalAdminJPanel11;
+import userinterface.HospitalRole.DoctorJPanel;
+
+/**
+ *
+ * @author junyaoli
+ */
+public class HospitalAdminRole extends Role{
+    
+    public HospitalAdminRole(String roleType,  int enterpriseID) {
+        super(roleType,  enterpriseID);
+    }
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, EcoSystem business) {
+        return new HospitalAdminJPanel11(userProcessContainer,account,business);//To change body of generated methods, choose Tools | Templates.
+    }
+}
---
+++ SaveAnimal_Final4.21/src/userinterface/SysAdmin/ManageEnterpriseJPanel.java
@@ -0,0 +1,203 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userinterface.SysAdmin;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Enterprise.EnterpriseDirectory;
+import Business.Network.Network;
+import Business.Network.NetworkDirectory;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author wang
+ */
+public class ManageEnterpriseJPanel extends JPanel implements ActionListener{
+    JTable jtable;
+    JScrollPane jsp;
+    JPanel p1, p2, p3, p4, p5;
+    JLabel p1_lab1;
+    JTextField p1_jtf1;
+    JButton p1_jb1,p3_jb1, p4_jb1, p4_jb2, p4_jb3, p4_jb4;
+    EcoSystem system;
+    JPanel userProcessContainer;
+    Network network;
+    
+    public ManageEnterpriseJPanel(JPanel userProcessContainer,EcoSystem system,Network network){
+        this.system = system;
+        this.userProcessContainer = userProcessContainer;
+        this.network=network;
+        this.ShopView();
+        populateRequestTable();
+    }
+    
+    public void populateRequestTable(){
+
+            DefaultTableModel dtm = (DefaultTableModel) jtable.getModel();
+            dtm.setRowCount(0);      
+            EnterpriseDirectory worklist= network.getEnterpriseDirectory();
+          
+            if (worklist != null){  
+                for(Enterprise request : worklist.getEnterpriseList()){
+                    Object[] row = new Object[2];
+                    row[0]=request.getEnterpriseID();
+                    row[1]=request.getEnterpriseName();
+//                    row[2]=request.getEnterpriseType();
+                    dtm.addRow(row);
+                }
+            }     
+    }
+    public void populateResearchTable(Enterprise request){
+
+            DefaultTableModel dtm = (DefaultTableModel) jtable.getModel();
+            dtm.setRowCount(0);          
+                    Object[] row = new Object[3];
+                    row[0]=request.getEnterpriseID();
+                    row[1]=request.getEnterpriseName();
+//                    row[2]=request.getEnterpriseType();
+                    dtm.addRow(row);                    
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //查询
+        if(e.getSource() == p1_jb1){
+            String search = p1_jtf1.getText();
+        Enterprise findAccount = network.getEnterpriseDirectory().findEnterprise(search);
+        //这里应该返回一个找到用户名字的List  //现在是返回一个网络个体
+        populateResearchTable(findAccount);
+        
+            //返回
+        }else if(e.getSource() == p3_jb1){            
+            userProcessContainer.remove(this);
+            Component[] componentArray = userProcessContainer.getComponents();
+            Component component = componentArray[componentArray.length - 1];
+            CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+            layout.previous(userProcessContainer);
+            //刷新信息
+        }else if(e.getSource() == p4_jb1){
+            populateRequestTable();    
+            
+            //增加
+        }else if(e.getSource() == p4_jb2){
+            System.out.println("增加");
+            
+            addManageEnterprise ad = new addManageEnterprise(network);
+            //system.getNetworkdirectory().getNetworkdirectory().add(new Network(addnamejTextField.getText()));
+            populateRequestTable();
+            
+            
+            //修改
+        }else if(e.getSource() == p4_jb3){
+                int row = jtable.getSelectedRow();
+        if (row < 0) {
+            JOptionPane.showMessageDialog(null,"Please select a row!", "get Request",JOptionPane.ERROR_MESSAGE);
+        } else{
+            //需要加入双重确认，并且改成以ID为搜索指引
+            String name = jtable.getValueAt(row,1).toString();
+            EnterpriseDirectory worklist= network.getEnterpriseDirectory();
+            Enterprise enterprise = worklist.findEnterprise(name);
+                   
+            CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+            userProcessContainer.add(new ManageDepartmentJPanel(userProcessContainer,system,network , enterprise));
+            layout.next(userProcessContainer);
+
+
+            }  
+            
+            //删除
+        }else if(e.getSource() == p4_jb4){
+                int row = jtable.getSelectedRow();
+                if (row < 0) {
+                    JOptionPane.showMessageDialog(null,"Please select a row!", "get Request",JOptionPane.ERROR_MESSAGE);
+                } else{
+                    //需要加入双重确认，并且改成以ID为搜索指引
+                    String name = jtable.getValueAt(row,1).toString();
+                    NetworkDirectory worklist= system.getNetworkdirectory();
+                    Network network = worklist.findNetWork(name);
+                    worklist.getNetworkdirectory().remove(network);          
+                }         
+                  populateRequestTable();    
+            }              
+   
+        }
+   
+    
+    public void ShopView(){
+        //上
+		p1 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		p1_lab1 = new JLabel("Manage Enterprise 请输入姓名（员工号或职位）");
+		p1_jtf1 = new JTextField(20);
+		p1_jb1 = new JButton("查询");
+		p1_jb1.addActionListener(this);
+		p1.add(p1_lab1);
+		p1.add(p1_jtf1);
+		p1.add(p1_jb1);
+		
+		//中
+		p2 = new JPanel(new BorderLayout());
+		jtable = new JTable();
+                jtable.setModel(new javax.swing.table.DefaultTableModel(
+                    new Object [][] {
+                        {null, null},
+                        {null, null},
+                        {null, null},
+                        {null, null}
+                    },
+                    new String [] {
+                        "ID", "Name"
+                    }));
+		jsp = new JScrollPane(jtable);
+		p2.add(jsp);
+		
+		
+		//下
+		p3 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		
+                p3_jb1 = new JButton("返回");
+                p3_jb1.addActionListener(this);
+		p3.add(p3_jb1);
+		
+		p4 = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		p4_jb1 = new JButton("刷新");
+		p4_jb1.addActionListener(this);
+		
+		p4_jb2 = new JButton("添加");
+		p4_jb2.addActionListener(this);
+		
+		p4_jb3 = new JButton("Manage Department");
+		p4_jb3.addActionListener(this);
+		
+		p4_jb4 = new JButton("删除");
+		p4_jb4.addActionListener(this);
+		
+		p4.add(p4_jb1);
+		p4.add(p4_jb2);
+		p4.add(p4_jb3);
+		p4.add(p4_jb4);
+		
+		p5 = new JPanel(new BorderLayout());
+		p5.add(p3,"West");
+		
+		p5.add(p4,"East");
+		
+		//设置整个布局为border
+		this.setLayout(new BorderLayout());
+		this.add(p1,"North");
+		this.add(p2,"Center");
+		this.add(p5,"South");
+		this.setVisible(true);
+        
+    }
+}
